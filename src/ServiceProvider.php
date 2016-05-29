12a5bf4a06a29be9329fdc6bdbc9f927b8c04776
Sayan <sayan652@gmail.com>
2016-05-29 20:14:24 +0530

    Update ServiceProvider.php
    Updating the config file name of Dompdf for inclusion.

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -36,7 +36,7 @@ class ServiceProvider extends IlluminateServiceProvider {
 
         $config_file = $this->app['config']->get(
             'laravel-dompdf::config_file'
-        ) ?: $this->app['path.base'] . '/vendor/dompdf/dompdf/dompdf_config.inc.php';
+        ) ?: $this->app['path.base'] . '/vendor/dompdf/dompdf/autoload.inc.php';
 
         if (file_exists($config_file)) {
             require_once $config_file;
